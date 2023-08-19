eaaca0122e0bff876901961a3b78a0e1a9b0dc7d
Manuella Barros <manubarrossa@gmail.com>
2023-08-19 17:12:07 -0300

    Teste json-server

--- src/lib/axios.tsx
+++ src/lib/axios.tsx
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export const axiosAPI = axios.create({
     //baseURL: 'http://localhost:3000'
-    baseURL: 'https://teste-vercel-virid.vercel.app'
+    //baseURL: 'https://teste-vercel-virid.vercel.app'
+    baseURL: 'https://dt-money-xi.vercel.app'
 })
