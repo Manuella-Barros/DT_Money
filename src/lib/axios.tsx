6bd7f1eb82afae194d6b2f41c6fc6effd5de4b67
Manuella Barros <manubarrossa@gmail.com>
2023-08-19 20:05:24 -0300

    resolvendo erros

--- src/lib/axios.tsx
+++ src/lib/axios.tsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export const axiosAPI = axios.create({
-    //baseURL: 'http://localhost:3000'
-    baseURL: 'https://teste-vercel-virid.vercel.app'
+    baseURL: 'http://localhost:3000'
 })
